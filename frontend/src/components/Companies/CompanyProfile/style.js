9b30867610f19c1657b8517e827aa988849cb625
Stefan Kosavic <stefankosavic@Stefans-MacBook-Pro.local>
2023-05-23 15:35:30 +0200

    initial company profile setup

---
+++ frontend/src/components/Companies/CompanyProfile/style.js
@@ -0,0 +1,22 @@
+import { createStyles, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    container: {
+      maxWidth:' 600px',
+      margin: 'auto',
+      padding: '20px',
+      color: 'red'
+    },
+    coverImage: {
+      backgroundImage: 'url(path/to/cover-image.jpg)',
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      height: '300px',
+      borderWidth: '1px',
+      borderColor: 'black'
+    },
+  })
+)
+
+export default useStyles

92cfc7dbb2beed974d1e50343c7db5093fba58f2
Stefan Kosavic <stefankosavic@Stefans-MacBook-Pro.local>
2023-05-23 16:53:40 +0200

    companyProfile setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
 import Registration from './components/auth/Registration';
 import LandingPage from './components/auth/LandingPage';
+import CompanyProfile from './components/Companies/CompanyProfile';
 import ToggleSwitch from './components/ToggleSwitch';
 
 import './App.css';
@@ -22,6 +23,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={LandingPage} />
           <Route path="/registracija" component={Registration} />
+          <Route path="/companyProfile" component={CompanyProfile} />
         </Switch>
       </Router>
 

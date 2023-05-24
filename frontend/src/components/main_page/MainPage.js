a8281f73595fcd077e0ed5d781089edf07fb824c
Amina <amina.dacic019@gmail.com>
2023-05-24 17:34:39 +0200

    Added Carousel component for the main page, also example images. And css files. Added example for card component.

--- frontend/src/components/main_page/MainPage.js
+++ frontend/src/components/main_page/MainPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navigation_Bar from "./NavigationBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselComp from "./Carousel";
+
 
 function MainPage() {
     return (
         <div className="main-page">
             <Navigation_Bar/>
+            <CarouselComp/>
         </div>
     );
 }

32d7778a8792ecb871742b40188492082a99372e
Amina <amina.dacic019@gmail.com>
2023-05-25 02:09:38 +0200

    Changed Card component

--- frontend/src/components/main_page/Cards.js
+++ frontend/src/components/main_page/Cards.js
@@ -1,22 +1,19 @@
 import Card from 'react-bootstrap/Card';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import {Button} from "react-bootstrap";
 
-function GridExample() {
+function CardComp() {
     return (
         <Card className="card">
             <Card.Img variant="top" src="holder.js/100px180" />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>Košarka - Dvorana 101</Card.Title>
                 <Card.Text>
-                    Some quick example text to build on the card title and make up the
-                    bulk of the card's content.
+                    Najsavremeniji teren za kvalitetnu igru za vas tim!
                 </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+                <Button variant="primary">Rezervišite termin</Button>
             </Card.Body>
         </Card>
     );
 }
 
-export default GridExample;
+export default CardComp;

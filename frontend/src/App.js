4d1b75bad914df4442f5732bd9ffab8949e7120b
Stefan Kosavic <stefankosavic@MUNEVERA-A-L.localdomain>
2023-05-31 20:01:09 +0200

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from  'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from  'react-router-dom';
 import Registration from './components/auth/Registration';
 import LandingPage from './components/auth/LandingPage';
 import CompanyProfile from './components/Companies/CompanyProfile';
 import EventsCalendar from './components/Companies/EventsCalendar/EventsCalendar';
 import ToggleSwitch from './components/ToggleSwitch';
+import Login from './components/auth/Login';
+
 
 import './App.css';
 import './stylesheet_auth.css'
 
 
+
 function App() {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  const toggleTheme = () => {
-    setIsDarkTheme(!isDarkTheme);
-  };
+    const toggleTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    };
 
+<<<<<<< HEAD
   return (
     <div className={`app ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
       <Router>
@@ -27,12 +31,34 @@ function App() {
           <Route path="/calendar" component={EventsCalendar} />
         </Switch>
       </Router>
+=======
+    var id = sessionStorage.getItem('id');
+>>>>>>> 39aa5bb4a45746bb0ab198f141de1fa2808d6e40
 
-      <div className="toggle-container">
-        <ToggleSwitch isDarkTheme={isDarkTheme} onToggle={toggleTheme} />
-      </div>
-    </div> 
-  );
+    return (
+        <div className={`app ${isDarkTheme ? 'dark-theme' : 'light-theme'}`}>
+            <Router>
+                <Switch>
+                    <Route path="/prijava">
+                        {!id && <Login /> }
+                        {id && <Redirect to="/" />}
+                    </Route>
+                    <Route path="/registracija">
+                        {!id && <Registration /> }
+                        {id && <Redirect to="/" />}
+                    </Route>
+                    <Route path="/">
+                        {id && <CompanyProfile/> }
+                        {!id && <LandingPage/>}
+                    </Route>
+                </Switch>
+            </Router>
+
+            <div className="toggle-container">
+                <ToggleSwitch isDarkTheme={isDarkTheme} onToggle={toggleTheme} />
+            </div>
+        </div> 
+    );
 }
 
 export default App;
---
+++ frontend/src/components/auth/ResetPassword.js
@@ -0,0 +1,43 @@
+import {React, useRef} from 'react';
+import axios from "axios";
+import {Form} from "react-bootstrap";
+import '../../stylesheet_auth.css';
+
+
+function ResetPassword(props){
+
+    const emailRef = useRef();
+
+    function resetujMail() {
+        var email = emailRef.current.value;
+
+        axios.post('http://127.0.0.1:8000/sportista/emailReset', {
+            email: email,
+        }).then((response)=>{
+            }, (error) => {
+                console.log(error);
+            }
+        );
+
+        props.onChange(true);
+    }
+
+    return(
+        <div className='reset-pass'>           
+            <form className='forma'>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>E-mail pomoću kojeg ste registrovani</Form.Label>
+                    <Form.Control type="email" ref={emailRef}/>
+                </Form.Group>
+                <button type="submit" className="reset-button submit-form" onClick={()=>resetujMail()}> 
+                        Resetuj lozinku
+                </button>
+            </form>
+        </div>
+        
+    );
+}
+
+export default ResetPassword;
+
+

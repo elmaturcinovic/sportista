4faa807c503efc7d59ac0a4b0e449e9d819ec63d
Elma Turčinović <elmaturcinovic1106@gmail.com>
2023-06-10 17:00:38 +0200

    User password change

--- frontend/src/components/user/ProfileComp.js
+++ frontend/src/components/user/ProfileComp.js
@@ -1,68 +1,69 @@
-import React from "react";
-import {useState} from 'react';
+import { useState, useEffect } from "react";
 import ChooseFileComp from "./ChooseFileComp";
 import ChangePasswordComp from "./ChangePasswordComp";
 import InterestsComp from "./InterestsComp"
 
-
 const ProfileComp = () => {
+  const [password, setPassword] = useState('');
 
-    const name = sessionStorage.getItem("name")
-    const lastname = sessionStorage.getItem("lastname")
-    const username = sessionStorage.getItem("username")
-    const email = sessionStorage.getItem("email")
-    const password = sessionStorage.getItem("password")
-    const pass_invisible = "*".repeat(password.length);
+  useEffect(() => {
+    const storedPassword = sessionStorage.getItem("password");
+    setPassword(storedPassword);
+  }, []);
 
-    const field = {
-        name: name,
-        lastname: lastname,
-        username: username,
-        email: email,
-        password: pass_invisible
-    }
+  const handlePasswordChange = (newPassword) => {
+    setPassword(newPassword);
+  };
 
-    return (
-        <div className="profile-div">
-            <h2 className="headline-profile">Moj profil</h2>
-            <table className="table-one">
-                <tfoot>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <ChangePasswordComp/>
-                        </td>
-                    </tr>
-                </tfoot>
-                <tbody>
-                    <tr>
-                        <td>Ime:</td>
-                        <td>{field.name}</td>
-                    </tr>
-                    <tr>
-                        <td>Prezime:</td>
-                        <td>{field.lastname}</td>
-                    </tr>
-                    <tr>
-                        <td>Korisnicko ime:</td>
-                        <td>{field.username}</td>
-                    </tr>
-                    <tr>
-                        <td>E-mail adresa:</td>
-                        <td>{field.email}</td>
-                    </tr>
-                    <tr>
-                        <td>Lozinka:</td>
-                        <td>{field.password}</td>
-                    </tr>
-                </tbody>
-            </table>
+  const name = sessionStorage.getItem("name");
+  const lastname = sessionStorage.getItem("lastname");
+  const username = sessionStorage.getItem("username");
+  const email = sessionStorage.getItem("email");
+  const pass_invisible = "*".repeat(password.length);
 
-            <InterestsComp/>
-            <ChooseFileComp/>
-            
-        </div>
-    );
-}
+  return (
+    <div className="profile-div">
+      <h2 className="headline-profile">Moj profil</h2>
+      <table className="table-one">
+        <tfoot>
+          <tr>
+            <td></td>
+            <td>
+              <ChangePasswordComp
+                password={password}
+                onPasswordChange={handlePasswordChange}
+              />
+            </td>
+          </tr>
+        </tfoot>
+        <tbody>
+          <tr>
+            <td>Ime:</td>
+            <td>{name}</td>
+          </tr>
+          <tr>
+            <td>Prezime:</td>
+            <td>{lastname}</td>
+          </tr>
+          <tr>
+            <td>Korisnicko ime:</td>
+            <td>{username}</td>
+          </tr>
+          <tr>
+            <td>E-mail adresa:</td>
+            <td>{email}</td>
+          </tr>
+          <tr>
+            <td>Lozinka:</td>
+            <td>{pass_invisible}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <InterestsComp />
+      <ChooseFileComp />
+    </div>
+  );
+};
 
 export default ProfileComp;

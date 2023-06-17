2c5a3f2fa534da9940164e9c0f4ad5a392abfe85
kosavicstefan <stefan@valens.dev>
2023-06-18 00:20:51 +0200

    status update

--- backend/sportista/views.py
+++ backend/sportista/views.py
@@ -305,11 +305,14 @@ def get_all_appointments_by_owner(request, owner_id):
         sport_halls = SportsHall.objects.filter(owner_id=owner_id)
         sport_hall_ids = sport_halls.values_list('id', flat=True)
         print(sport_hall_ids)
-        
+
         appointments = Appointment.objects.filter(sport_hall_id__in=sport_hall_ids)
         appointment_data = []
 
         for appointment in appointments:
+            user_appointments = UserAppointment.objects.filter(appointment=appointment)
+            available_spots = appointment.capacity - sum(user_appointment.used_spots for user_appointment in user_appointments)
+
             appointment_data.append({
                 'id': appointment.id,
                 'sport_hall': appointment.sport_hall.name,
@@ -318,13 +321,15 @@ def get_all_appointments_by_owner(request, owner_id):
                 'time_start': appointment.time_start,
                 'time_end': appointment.time_end,
                 'capacity': appointment.capacity,
-                'price': appointment.price
+                'price': appointment.price,
+                'available': appointment.available and available_spots > 0
             })
 
         return Response(appointment_data)
 
     except SportsHall.DoesNotExist:
         return Response({'error': 'Owner does not exist'}, status=404)
+
 
 
 @api_view(['POST'])

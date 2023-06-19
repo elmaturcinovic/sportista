2d09c3a9597bac49d6eb0cca5c931d53ea8cdcd9
Elma Turčinović <elmaturcinovic1106@gmail.com>
2023-06-19 03:16:36 +0200

    Removed api key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -154,8 +154,8 @@ LOGGING = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587  # Use port 465 if using SSL
-EMAIL_HOST_USER = 'my-api-key'
-EMAIL_HOST_PASSWORD = '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'
+EMAIL_HOST_USER = 'my-second-api'
+EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = 'sportista.management@gmail.com'
 
 EMAIL_USE_TLS = True
